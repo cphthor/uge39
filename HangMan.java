78e616962c99bacb3564f7515cf57ba547beec47
Thor <thor@10.14.6>
2019-09-24 00:11:29 +0200

    opgaver uge 39 med løsninger

--- HangMan.java
+++ HangMan.java
@@ -1,4 +1,19 @@
 package numberguess;
+/*
+* FORMÅL:
+* At læse en større programstump og kunne tilpasse den
+
+* OPGAVEN:
+* Hangman-programmet virker nogenlunde men er langt fra perfekt.
+* 1) Sørg for at SecretWord rummer mindst 4 karakterer
+* 2) Sørg for at tjekket for om en karaktér er i ordet sikrer hvis den
+* forekommer flere gange
+* 3) Ordet som brugeren bygger med de rigtig gættede karakterer bruges ikke 
+* så man kan ikke se hvor langt man er.
+* 4) De forkerte bogstaver gemmes ikke. Overvej at sende dem med som parameter
+* til figuren og print dem som en ekstra linje i bunden.
+*
+*/
 public class HangMan {
 	public static void main(String[] args) {
 		String secretWord = ArrayHelper.getSecretWord();
@@ -8,7 +23,7 @@ public class HangMan {
 		char[] myWord= new char[secretWord.length()];
 		while (counter < 8) {
 			char userChar = ArrayHelper.getUserChar();
-			int idx =secretWord.indexOf(userChar); 
+			int idx = secretWord.indexOf(userChar);
 			if (idx >= 0) {
 				System.out.println("is found at " + idx);
 			} else {
@@ -20,100 +35,91 @@ public class HangMan {
 	public static void printFigure(int step) {
 		String format = "";
 		String myEmpty = " ";
-		/*
-		for(int i=0;i<6;i++){
-			format = "|%4s%n";
-			if (i==step){
-				format = myformat;
-			}
-				System.out.printf(format, myEmpty);
-		}
-		*/
 		if (step==1){
-		format = "______%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s|%n";
-		System.out.printf(format, myEmpty);
-		for(int i=0;i<3;i++) {
-		format = "|%4s%n";
-		System.out.printf(format, myEmpty);
-		}
+			format = "______%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s|%n";
+			System.out.printf(format, myEmpty);
+			for(int i=0;i<3;i++) {
+				format = "|%4s%n";
+				System.out.printf(format, myEmpty);
+			}
 		} else if (step == 2) {
-		format = "______%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s|%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4sO%n";
-		System.out.printf(format, myEmpty);
-		for(int i=0;i<2;i++) {
-		format = "|%4s%n";
-		System.out.printf(format, myEmpty);
-		}
+			format = "______%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s|%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4sO%n";
+			System.out.printf(format, myEmpty);
+			for(int i=0;i<2;i++) {
+				format = "|%4s%n";
+				System.out.printf(format, myEmpty);
+			}
 		} else if (step==3) {
-		format = "______%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s|%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4sO%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s|%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s%n";
-		System.out.printf(format, myEmpty);
+			format = "______%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s|%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4sO%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s|%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s%n";
+			System.out.printf(format, myEmpty);
 		} else if (step==4) {
-		format = "______%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s|%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4sO%n";
-		System.out.printf(format, myEmpty);
-		format = "|%3s/|%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s%n";
-		System.out.printf(format, myEmpty);
+			format = "______%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s|%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4sO%n";
+			System.out.printf(format, myEmpty);
+			format = "|%3s/|%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s%n";
+			System.out.printf(format, myEmpty);
 		} else if (step==5) {
-		format = "______%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s|%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4sO%n";
-		System.out.printf(format, myEmpty);
-		format = "|%3s/|\\%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s%n";
-		System.out.printf(format, myEmpty);
+			format = "______%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s|%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4sO%n";
+			System.out.printf(format, myEmpty);
+			format = "|%3s/|\\%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s%n";
+			System.out.printf(format, myEmpty);
 		} else if (step==6) {
-		format = "______%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s|%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4sO%n";
-		System.out.printf(format, myEmpty);
-		format = "|%3s/|\\%n";
-		System.out.printf(format, myEmpty);
-		format = "|%3s/%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s%n";
-		System.out.printf(format, myEmpty);
+			format = "______%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s|%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4sO%n";
+			System.out.printf(format, myEmpty);
+			format = "|%3s/|\\%n";
+			System.out.printf(format, myEmpty);
+			format = "|%3s/%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s%n";
+			System.out.printf(format, myEmpty);
 		} else if (step==7) {
-		format = "______%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s|%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4sO%n";
-		System.out.printf(format, myEmpty);
-		format = "|%3s/|\\%n";
-		System.out.printf(format, myEmpty);
-		format = "|%3s/ \\%n";
-		System.out.printf(format, myEmpty);
-		format = "|%4s%n";
-		System.out.printf(format, myEmpty);
-	}
+			format = "______%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s|%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4sO%n";
+			System.out.printf(format, myEmpty);
+			format = "|%3s/|\\%n";
+			System.out.printf(format, myEmpty);
+			format = "|%3s/ \\%n";
+			System.out.printf(format, myEmpty);
+			format = "|%4s%n";
+			System.out.printf(format, myEmpty);
+		}
 	}
 }
